655b1846812d78373407b43f6781386004fcd295
porChe1223 <houshinri@gmail.com>
2025-01-07 02:08:31 +0900

    Change: 念のため変数を変更

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ RESULT_PATH = './DataResult_Speak'
 RESULT_AVI_PATH = './DataResult_Speak/speak1.avi'
 RESULT_MP4_PATH = './DataResult_Speak/speak1.mp4'
 
-# # OpenFaceを実行
-# cmd = './OpenFace/build/bin/FaceLandmarkVidMulti -f DATA_PATH -out_dir RESULT_AVI_PATH'
-# cmd = cmd.replace('DATA_PATH', DATA_PATH)
-# cmd = cmd.replace('RESULT_AVI_PATH', RESULT_PATH)
-# subprocess.run(cmd, shell=True)
+# OpenFaceを実行
+cmd_1 = './OpenFace/build/bin/FaceLandmarkVidMulti -f DATA_PATH -out_dir RESULT_AVI_PATH'
+cmd_1 = cmd_1.replace('DATA_PATH', DATA_PATH)
+cmd_1 = cmd_1.replace('RESULT_AVI_PATH', RESULT_PATH)
+subprocess.run(cmd_1, shell=True)
 
 # AVIファイルをMP4ファイルに変換
-cmd = 'ffmpeg -i RESULT_AVI_PATH -vcodec h264 RESULT_MP4_PATH'
-cmd = cmd.replace('RESULT_AVI_PATH', RESULT_AVI_PATH)
-cmd = cmd.replace('RESULT_MP4_PATH', RESULT_MP4_PATH)
-subprocess.run(cmd, shell=True)
+cmd_2 = 'ffmpeg -i RESULT_AVI_PATH -vcodec h264 RESULT_MP4_PATH'
+cmd_2 = cmd_2.replace('RESULT_AVI_PATH', RESULT_AVI_PATH)
+cmd_2 = cmd_2.replace('RESULT_MP4_PATH', RESULT_MP4_PATH)
+subprocess.run(cmd_2, shell=True)
